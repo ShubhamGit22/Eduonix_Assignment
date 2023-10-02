d74c58d349120b654ceaca8d7702b9afd0910c5a
Shubham Ashok Chavan <139915555+ShubhamGit22@users.noreply.github.com>
2023-10-03 02:05:24 +0530

    Add files via upload

---
+++ p2 index.js
@@ -0,0 +1,71 @@
+//Que.1] Write a JavaScript program that asks the user for their age. If the user is over 18, the program should display the message "You are an adult."
+// Otherwise, the program should display the message "You are a minor."
+
+// let age = parseInt(prompt("What is your age?"));
+
+// if (age >= 18) {
+//   console.log("You are an adult.");
+// } else {
+//   console.log("You are a minor.");
+// }
+
+
+
+
+// Que.2] Write a JavaScript program that asks the user for their username and password. If the username and password are correct, the program should display the message "Login successful."
+// Otherwise, the program should display the message "Login failed."
+
+// let username = prompt("Enter your username:");
+// let password = prompt("Enter your password:");
+
+// let correctUsername = "admin";
+// let correctPassword = "password";
+
+// if (username === correctUsername && password === correctPassword) {
+//   console.log("Login successful");
+// } else {
+//   console.log("Login failed");
+// }
+
+
+
+
+// Que.3] Write a JavaScript program that generates a random number between 1 and 100. If the random number is even, the program should display the message "The number is even."
+//  Otherwise, the program should display the message "The number is odd."
+
+// let Number = parseInt(prompt("Enter the number"));
+
+// if (Number % 2 === 0) {
+//   console.log("The number is even");
+// } else {
+//   console.log("The number is odd");
+// }
+
+
+
+
+// Que. 4] Write a JavaScript program that asks the user for their favorite color. If the user's favorite color is red, the program should display the message "Red is a beautiful color." 
+// Otherwise, the program should display the message "I like your choice of color."
+
+// let favColor = prompt("What is your favorite color?");
+
+// if (favColor === "red") {
+//   console.log("Red is a beautiful color");
+// } else {
+//   console.log("I like your choice of color");
+// }
+
+
+
+
+//Que.4] Write a JavaScript program that asks the user for their grade in school. If the user's grade is A, the program should display the message "Congratulations on your excellent work!" 
+// Otherwise, the program should display the message "Keep working hard!"
+// Ask the user for their grade in school
+
+// let grade = prompt("What is your grade in school?");
+
+// if (grade === "A") {
+//   console.log("Congratulations on your excellent work!");
+// } else {
+//   console.log("Keep working hard!");
+// }
