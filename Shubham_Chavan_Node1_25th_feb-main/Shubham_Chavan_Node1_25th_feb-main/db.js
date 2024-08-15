2d80b724bbe96521d867903a8f4d277836c83364
Shubham Ashok Chavan <139915555+ShubhamGit22@users.noreply.github.com>
2024-08-15 13:00:31 +0530

    Delete Shubham_Chavan_Node1_25th_feb-main/Shubham_Chavan_Node1_25th_feb-main directory

--- Shubham_Chavan_Node1_25th_feb-main/Shubham_Chavan_Node1_25th_feb-main/db.js
+++
@@ -1,5 +0,0 @@
-// npm install mongoose 2nd step
-const mongoose=require("mongoose")
-mongoose.connect("mongodb://0.0.0.0:27017/login")//folder name is practice 
-// export default function name this in react
-module.exports=mongoose;
